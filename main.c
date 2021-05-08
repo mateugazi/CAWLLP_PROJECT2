62a0f02b8ec2de92b456d7d7fbe604fc32457173
cichy30002 <cichy30002@gmail.com>
2021-05-08 13:34:33 +0200

    4.0 goals accomplished, 4.5 still have some bugs

--- main.c
+++ main.c
@@ -36,28 +36,45 @@ typedef struct tagBITMAPINFOHEADER {
 
 int main(int argc, char *argv[]) {
     FILE *file = fopen(argv[1], "r");
-
+    FILE *outfile;
+    if(argv[2]!= NULL) outfile = fopen(argv[2], "w");
     struct tagBITMAPFILEHEADER headerFile;
     struct tagBITMAPINFOHEADER headerInfo;
 
     if(file != NULL){
         fread(&headerFile.bfType, 2, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerFile.bfType, 2, 1, outfile);
         fread(&headerFile.bfSize, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerFile.bfSize, 4, 1, outfile);
         fread(&headerFile.bfReserved1, 2, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerFile.bfReserved1, 2, 1, outfile);
         fread(&headerFile.bfReserved2, 2, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerFile.bfReserved2, 2, 1, outfile);
         fread(&headerFile.bfOffBits, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerFile.bfOffBits, 4, 1, outfile);
 
         fread(&headerInfo.biSize, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biSize, 4, 1, outfile);
         fread(&headerInfo.biWidth, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biWidth, 4, 1, outfile);
         fread(&headerInfo.biHeight, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biHeight, 4, 1, outfile);
         fread(&headerInfo.biPlanes, 2, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biPlanes, 2, 1, outfile);
         fread(&headerInfo.biBitCount, 2, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biBitCount, 2, 1, outfile);
         fread(&headerInfo.biCompression, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biCompression, 4, 1, outfile);
         fread(&headerInfo.biSizeImage, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biSizeImage, 4, 1, outfile);
         fread(&headerInfo.biXPelsPerMeter, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biXPelsPerMeter, 4, 1, outfile);
         fread(&headerInfo.biYPelsPerMeter, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biYPelsPerMeter, 4, 1, outfile);
         fread(&headerInfo.biClrUsed, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biClrUsed, 4, 1, outfile);
         fread(&headerInfo.biClrImportant, 4, 1, file);
+        if(argv[2]!= NULL)fwrite(&headerInfo.biClrImportant, 4, 1, outfile);
 
 
         printf("BITMAPFILEHEADER: \n");
@@ -109,8 +126,62 @@ int main(int argc, char *argv[]) {
         printf("\n");
         printf("biClrImportant: \t");
         printf("%"PRIu32, headerInfo.biClrImportant);
+        printf("\n");
     } else {
-        printf("Wrong file");
+        printf("Wrong file\n");
     }
+    if(headerInfo.biCompression==0 && headerInfo.biBitCount == 24)
+    {
+        int rowLength = (headerInfo.biBitCount*headerInfo.biWidth+31) / 32;
+        rowLength *=4;
+        int rowToProcess = headerInfo.biWidth*3;
+        int rgb [3][16];
+        for (int j = 0; j < 16; ++j) {
+            rgb[0][j]=0;
+            rgb[1][j]=0;
+            rgb[2][j]=0;
+        }
+        unsigned char ffs [rowLength];
+        fread(ffs, 1, rowLength,file);
+        if(argv[2]!= NULL)fwrite(ffs, 1, rowLength,outfile);
+        for(int i=0;i<headerInfo.biHeight;i++)
+        {
+            unsigned char row [rowLength];
+            unsigned char greyRow [rowLength];
+            for (int j = 0; j < rowLength; ++j) {
+                greyRow[j]=0;
+            }
+            fread(row, 1, rowLength,file);
+
+            for (int j = 0; j < rowToProcess; j+=3) {
+                rgb[0][row[j] / 16]++;
+                rgb[1][row[j + 1] / 16]++;
+                rgb[2][row[j + 2] / 16]++;
+                int avg = (row[j]+row[j+1]+row[j+2])/3;
+                greyRow[j]=avg;
+                greyRow[j+1]=avg;
+                greyRow[j+2]=avg;
+            }
+            if(argv[2]!= NULL)fwrite(greyRow, 1, rowLength,outfile);
+        }
+        float div = rowToProcess/3*headerInfo.biHeight/100;
+        printf("Red:\n");
+        for (int i = 0; i < 16; ++i) {
+            printf("%d-%d: %2.2f%%\n",i*16,i*16+15,rgb[0][i]/div);
+        }
+        printf("Green:\n");
+        for (int i = 0; i < 16; ++i) {
+            printf("%d-%d: %2.2f%%\n",i*16,i*16+15,rgb[1][i]/div);
+        }
+        printf("Blue:\n");
+        for (int i = 0; i < 16; ++i) {
+            printf("%d-%d: %2.2f%%\n",i*16,i*16+15,rgb[2][i]/div);
+        }
+
+    } else{
+        printf("histogram calculation is unsupported\n");
+    }
+    fclose(file);
+    fclose(outfile);
     return 0;
 }
